2aa005ec005ece4d4024c066d138631a0731df27
poiupio <poiupio@hotmail.com>
2019-11-19 11:35:27 -0600

    Re-uploaded OOP1 1

--- oop1.rb
+++ oop1.rb
@@ -1,12 +1,12 @@
 class usuario
 attr_accessor :name, :last_name
 
-  def initialize(name, last_name)
-    @name = name
-    @last_name = last_name
-  end
+def initialize(name, last_name)
+  @name = name
+  @last_name = last_name
+end
 
-  def generate_full_name
-    full_name = @name + @last_name
-  end
+def full_name
+  "#{@name} #{@last_name}"
+end
 end
