3b053563b1b6bb085c9074a668000b6c0d64cd2d
poiupio <poiupio@hotmail.com>
2019-11-20 10:17:29 -0600

    Car objet with start stop and travel metods

---
+++ car.rb
@@ -0,0 +1,25 @@
+class Car
+  state = 'stop'
+
+  def initialize (brand, year, color)
+    @brand = brand
+    @year = year
+    @color = color
+  end
+
+  def start
+    raise('The car must be stoped to start') unless @state == 'stop'
+    @state = 'started'
+  end
+
+  def travel
+    raise('The car must be started to travel') unless @state == 'started'
+    @state = 'traveling'
+ 
+  end
+
+  def stop
+    raise('the car must be started or in travel to stop') unless @state == ('started' || 'traveling')
+    @state = 'stop'
+  end
+end
