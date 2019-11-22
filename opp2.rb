1f832d15c58852ed70a3a8cc72cf695a28645f1c
poiupio <poiupio@hotmail.com>
2019-11-22 12:28:40 -0600

    Update opp2.rb

--- opp2.rb
+++ opp2.rb
@@ -1,17 +1,17 @@
 class Contact
-  def initialize
-    @contacts = [
+  @@contacts = [
       { age: 20, name: 'Jose', last_name: 'Lopez', sex: :masculino },
       { age: 21, name: 'Oscar', last_name: 'Perez', sex: :masculino},
       { age: 22, name: 'Martha', last_name: 'Sanchez', sex: :femenino}
     ]
-  end
 
   def self.all 
-    @contacts
+    @@contacts
   end
 
   def self.find_by(key, param)
-    @contacts.select { |contact| contact[key] == param }
+    @@contacts.select { |contact| contact[key] == param }
   end
 end
+
+p Contact.all
