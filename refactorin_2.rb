37f401eb9fc41fa18682ba6d7ab7c962a21db0b4
poiupio <poiupio@hotmail.com>
2019-11-22 12:01:37 -0600

    Update refactorin_2.rb

--- refactorin_2.rb
+++ refactorin_2.rb
@@ -1,5 +1,5 @@
-def generate_name
-  name=fullname
+def long_name
+  name = fullname
   return name unless organization_id
   organization = Organization.lookup(id: organization_id)
   return name += " (#{organization.name})" if organization
