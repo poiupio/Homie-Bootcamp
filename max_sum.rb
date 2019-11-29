d6ff286865a515eba4a1d0ea0e8da20943d00ce0
poiupio <poiupio@hotmail.com>
2019-11-28 18:35:44 -0600

    Update max_sum.rb

--- max_sum.rb
+++ max_sum.rb
@@ -7,17 +7,24 @@ end
 
 def max_sum_columns(array_of_arrays)
   array_transpose = array_of_arrays.transpose[1]
-  p array_transpose.inject(:+)
+  array_transpose.inject(:+)
 end
 
 def max_sum_rows(array_of_arrays)
   max_sum = 0
+  
   array_of_arrays.each do |array|
     array_sum = array.sum
     max_sum = (max_sum > array_sum ? max_sum : array_sum)
   end
-  p max_sum
+  
+  max_sum
 end
 
-A = [[6,7,8], [1,4,29], [4,78,12]]
+A = [
+     [6, 7, 8], 
+     [1, 4, 29], 
+     [4, 78, 12]
+    ]
+
 p max_sum(A)
