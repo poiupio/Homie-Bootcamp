58e71a81fdcd8593b3dc545026ee71bb32cf8b76
poiupio <poiupio@hotmail.com>
2019-12-13 16:00:41 -0600

    added workplace to user

--- blog/app/controllers/users_controller.rb
+++ blog/app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :name, :last_name, :mobile_phone)
+      params.require(:user).permit(:email, :name, :last_name, :mobile_phone, :work_place)
     end
 end
--- blog/app/views/users/_user.json.jbuilder
+++ blog/app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :email, :name, :last_name, :mobile_phone, :created_at, :updated_at
+json.extract! user, :id, :email, :name, :last_name, :mobile_phone, work_place, :created_at, :updated_at
 json.url user_url(user, format: :json)
