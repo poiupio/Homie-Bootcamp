c47dee21ce6eb6fa788d4a654969530c0592744e
poiupio <poiupio@hotmail.com>
2019-12-19 11:37:58 -0600

    WIP tests user

--- blog/spec/models/user_spec.rb
+++ blog/spec/models/user_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   it "is valid with valid attributes" do
-    user = User.new(name: "Daniel")
+    user = User.new
+    user.email = FFaker::Internet.email
+    user.name = FFaker::Name.first_name
+    user.last_name = FFaker::Name.last_name
+    user.mobile_phone = FFaker::PhoneNumber
+    user.work_place = "Homie <3"
+    
     expect(user).to be_valid
   end
 
@@ -11,8 +18,7 @@ RSpec.describe User, type: :model do
     expect(user).to_not be_valid
   end
 
-  it "Exist associations with rents" do
-    user = User.new(name: "Daniel")
-    expect(user).to validates_presence_of :rent
+  it "Validate presence of name" do
+    should validate_presence_of(:name)
   end
 end
