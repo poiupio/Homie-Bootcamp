805b5290b3b9c4bebbc85da6d2591bbfe9167231
poiupio <poiupio@hotmail.com>
2019-12-19 11:37:58 -0600

    WIP tests user

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,11 +1,13 @@
 class User
   include Mongoid::Document
-  has_many :rents
 
-  validates :name, presence: { message: 'Se requiere nombre' }
   field :email, type: String
   field :name, type: String
   field :last_name, type: String
   field :mobile_phone, type: String
   field :work_place, type: String
+ 
+  validates :name, presence: { message: 'Se requiere nombre' }
+ 
+  has_many :rents
 end
