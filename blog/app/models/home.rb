a8f7f73e2bb3d63604b5a27fd87eadaf0ee090da
poiupio <poiupio@hotmail.com>
2019-12-20 11:29:55 -0600

    WIP test for user, rent and home

--- blog/app/models/home.rb
+++ blog/app/models/home.rb
@@ -1,13 +1,17 @@
 class Home
   include Mongoid::Document
-  belongs_to :owner
-  has_many :rents
-
+  
   field :price, type: Float
-  field :extra_service, type: String
   field :total_amount, type: Float
+  field :extra_service, type: String, default: "No"
+  field :home_features, type: HASH, default: { garden: false, furnished:false, gym:false }
+  
   validates :home_features[:garden], inclusion: [true, false]
   validates :home_features[:furnished], inclusion: [true, false]
   validates :home_features[:gym], inclusion: [true, false]
-  field :home_features, type: HASH, default: { garden: false, furnished:false, gym:false }
+  validates :price, presence: { message: 'Se requiere precio' }
+  validates :total_amount, presence: { message: 'Se requiere cantidad total' }
+
+  belongs_to :owner
+  has_many :rents
 end
--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -7,7 +7,11 @@ class User
   field :mobile_phone, type: String
   field :work_place, type: String
  
+  validates :email, presence: { message: 'Se requiere email' }
   validates :name, presence: { message: 'Se requiere nombre' }
+  validates :last_name, presence: { message: 'Se requiere apellidos' }
+  validates :mobile_phone, presence: { message: 'Se requiere telefono' }
+  validates :work_place, presence: { message: 'Se requiere lugar de trabajo' }
  
   has_many :rents
 end
