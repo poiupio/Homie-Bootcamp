f5f29f565c44798b2b21ea03a7ddab4177a5ba58
poiupio <poiupio@hotmail.com>
2019-12-26 16:02:49 -0600

    started the react project

--- hola_mundo/src/App.js
+++ hola_mundo/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ¡Hola mundo!
         </a>
       </header>
     </div>
