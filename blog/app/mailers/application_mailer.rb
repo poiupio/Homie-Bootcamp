e40bc95c8d0d6587dccd73c28f0de7541dca1a0a
poiupio <poiupio@hotmail.com>
2019-12-26 11:31:24 -0600

    fixed things

--- blog/app/mailers/application_mailer.rb
+++ blog/app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  add_template_helper(EmailHelper)
   default from: 'Homie@example.com'
   layout 'mailer'
 end
--- blog/config/environments/development.rb
+++ blog/config/environments/development.rb
@@ -51,5 +51,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  config.action_controller.asset_host = "http://localhost:3000/"
+  config.action_mailer.asset_host = config.action_controller.asset_host
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
