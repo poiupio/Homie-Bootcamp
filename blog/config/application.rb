affbbf1d01d412b42b0042bf01a1e6711b6269ac
poiupio <poiupio@hotmail.com>
2020-01-06 14:53:40 -0600

    Entrega 6 enero

--- blog/config/application.rb
+++ blog/config/application.rb
@@ -30,7 +30,6 @@ module Blog
     config.generators do |g|
       g.orm :active_record
     end
-    config.factory_bot.definition_file_paths = ["spec/factories"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
