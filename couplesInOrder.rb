4487fbad91634e8cb135fdd199cc8e986bffe233
poiupio <poiupio@hotmail.com>
2020-01-07 16:24:39 -0600

    Update couplesInOrder.rb

--- couplesInOrder.rb
+++ couplesInOrder.rb
@@ -2,9 +2,9 @@ def alphabetic_order (couples)
   couples.each do |couple| 
     lower_couple = couple.upcase
     lower_comparator = (couple.upcase).chars.sort.join
-    return "#{couple} isnt consecutive" unless (lower_couple.eql? lower_comparator)
+    return false unless (lower_couple.eql? lower_comparator)
   end
-  "All the couples are consecutive"
+  true
 end
 
 p alphabetic_order(["AB", "bc", "Dc"])
